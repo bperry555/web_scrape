689ae28bbe593751aaf8f3a8fd37853854899619
Brian Perry <blperry5@gmail.com>
2020-03-30 18:14:02 -0700

    working

--- app.py
+++ app.py
@@ -5,33 +5,31 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
+def scrape():
+
+    # Run the scrape function
+    mars_facts = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_db.replace_one({}, mars_facts, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/home")
+
+@app.route("/home")
 def home():
 
     # Find one record of data from the mongo database
-    # mars_info = mongo.db.mars_info.find_one()
+    mars_db_info = mongo.db.mars_db.find_one()
 
     # Return template and data
-    return render_template("index.html")
+    return render_template('index.html', mars_db_info = mars_db_info)
     #  mars=mars_info)
 
-# Route that will trigger the scrape function
-# @app.route("/scrape")
-# def scrape():
-
-#     # Run the scrape function
-#     mars_data = scrape_mars.scrape_info()
-
-#     # Update the Mongo database using update and upsert=True
-#     mongo.db.collection.update({}, mars_data, upsert=True)
-
-#     # Redirect back to home page
-#     return redirect("/")
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -4,32 +4,28 @@ import pandas as pd
 import time
 import requests
 
-def scrape_info():
-    browser = Browser()
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
-    html = requests.get('https://mars.nasa.gov/news')  
+def scrape():
+    # browser = Browser()
+
+    html = requests.get('https://mars.nasa.gov/news/')
     soup = bs(html.text,'html.parser')
 
-    nasa_headline = []
-    nasa_text = []
-
-    for headline in soup.find_all('div', class_='content_title'):
-        nasa_headline.append(headline.get_text(strip=True))
-    for text in soup.find_all('div', class_='rollover_description_inner'):
-        nasa_text.append(text.get_text(strip=True))
-
-    nasa_news = dict(zip(nasa_headline, nasa_text))
+    nasa_news = soup.find('div', class_='content_title').text.strip('\n')
+    nasa_article = soup.find('div', class_='rollover_description_inner').text.strip('\n')
 
 
-    executable_path = {'executable_path': '/'}
-    browser = Browser('chrome', executable_path)
-    browser.visit('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
-    soup = bs(browser.html, 'html.parser')
+    # browser = Browser('chrome', init_browser())
+    # browser.visit('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+    browser = requests.get('https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
+    soup = bs(browser.text, 'html.parser')
+    # browser.quit()
     result = soup.find('article')['style']
-    full_featured_img = "https://www.jpl.nasa.gov" + result.split(' ')[1].strip("url('');")
-    browser.quit()
-
-    featured_img = {'Feature Image': full_featured_img}
+    featured_img_url = "https://www.jpl.nasa.gov" + result.split(' ')[1].strip("url('');")
     
     
     html = requests.get('https://twitter.com/marswxreport?lang=en')
@@ -37,23 +33,24 @@ def scrape_info():
 
     import re
 
-    mars_weather = []
+    mars_tweet = []
+
     tweets = soup.find_all('div', {'class':'tweet'})
     for tweet in tweets:
-        # context = tweet.find('div',{'class':'context'}).text.replace("\n"," ").strip()
+        # context = tweet.find('div',{'class':'context'}).text.replace("\n"," ").strip()    *Might Use Later*
         content = tweet.find('div',{'class':'content'})
-        # header = content.find('div',{'class':'stream-item-header'})
-        # user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()     
-        # time = header.find('a',{'class':'tweet-timestamp js-permalink js-nav js-tooltip'}).find('span').text.replace("\n"," ").strip()
+        # header = content.find('div',{'class':'stream-item-header'})   *Might Use Later*
+        # user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()   *Might Use Later*   
+        # time = header.find('a',{'class':'tweet-timestamp js-permalink js-nav js-tooltip'}).find('span').text.replace("\n"," ").strip()    *Might Use Later*
         message = content.find('div',{'class':'js-tweet-text-container'}).text.replace("\n"," ").strip()
         
         match = re.search('InSight', message)
         if match:
-            mars_weather.append(message)
+            mars_tweet.append(message)
+    mars_weather_tweet = mars_tweet[0] 
 
     df = pd.read_html('https://space-facts.com/venus/')
-
-    mars_table = {'table': df[0].to_html()}
+    mars_table = df[0].to_html()
 
 
     html_list = ['https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced',
@@ -61,19 +58,23 @@ def scrape_info():
              'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced',
              'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced']
              
-
+    mars_hemisphere_image_urls = []
+    mars_hemisphere_image_urls.clear()
+                
     for html in html_list:
         response = requests.get(html)
         soup = bs(response.text, 'html.parser')
+        
         title_result = soup.find('h2', class_='title').text
-        pic_result = str(soup.find('a',text='Original')).split()[1].lstrip('href=').strip('""')
-        pict_title = {'pic_title': title_result}
-        pict_html = {'pic_html': pic_result}
+        pic_result = 'https://astrogeology.usgs.gov' + soup.find('img',class_='wide-image')['src']
+        pic_dict = {'title': title_result, 'img_url': pic_result}
+        mars_hemisphere_image_urls.append(pic_dict)
     
-    scrape_info = {{'news': nasa_news},
-                    {'img': featured_img},
-                    {'table': mars_table},
-                    {'pict title': pict_title},
-                    {'pict html' : pict_html}}
-    
-    return(scrape_info)
+    mars_facts = {'nasa_news': nasa_news,
+             'nasa_article': nasa_article,
+             'featured_img_url': featured_img_url,
+             'mars_weather_tweet': mars_weather_tweet,
+             'mars_table': mars_table,
+             'mars_hemisphere_image_urls': mars_hemisphere_image_urls}
+
+    return(mars_facts)
